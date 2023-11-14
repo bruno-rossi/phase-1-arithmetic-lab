f7f56326af52818777f981e4cba47c323aaaf283
Bruno Rossi <brunogabrielrossi@gmail.com>
2023-11-14 13:54:29 -0500

    Submitting Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
+
 // Write your code here
+
+let num1 = 31;
+
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+
+// Math.random() returns a number between 0 and 0.999..., which we multiply by 10 to give us a number between 0 and 9.999.... We then pass that number to Math.floor(), which returns an integer between 0 and 9. That's one less than the desired range (1 to 10), so we add one at the end of the equation:
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 8;
+
+let num4 = 2;
+
+let mod = num3 / num4;
+
+let max = Math.max(10, 15, 20);
+
